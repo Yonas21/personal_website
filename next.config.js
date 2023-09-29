55b92b6617e3365dcccd7362c6851813649915e6
Yonas21 <yonalem21@gmail.com>
2023-09-29 22:44:29 +0300

    email server added

--- next.config.js
+++ next.config.js
@@ -24,6 +24,11 @@ const nextConfig = {
     multipass: true,
     plugins: ['removeDimensions'],
   },
+  env: {
+    SERVICE_ID: process.env.SERVICE_ID,
+    TEMPLATE_ID: process.env.TEMPLATE_ID,
+    USER_ID: process.env.USER_ID,
+  },
   strictMode: true,
   swcMinify: true,
   trailingSlash: false,
--- src/components/Sections/Contact/ContactForm.tsx
+++ src/components/Sections/Contact/ContactForm.tsx
@@ -1,16 +1,18 @@
+import emailjs from 'emailjs-com';
 import {FC, memo, useCallback, useMemo, useState} from 'react';
 
+const {SERVICE_ID, TEMPLATE_ID, USER_ID} = process.env;
 interface FormData {
-  name: string;
-  email: string;
+  from_name: string;
+  from_email: string;
   message: string;
 }
 
 const ContactForm: FC = memo(() => {
   const defaultData = useMemo(
     () => ({
-      name: '',
-      email: '',
+      from_name: '',
+      from_email: '',
       message: '',
     }),
     [],
@@ -29,6 +31,15 @@ const ContactForm: FC = memo(() => {
     [data],
   );
 
+  // eslint-disable-next-line react-memo/require-memo
+  const Success = () => {
+    <div className="border-b border-t border-blue-500 bg-blue-100 px-4 py-3 text-blue-700" role="alert">
+      <p className="font-bold">Success</p>
+      <p className="text-sm">Sent!!</p>
+    </div>;
+  };
+
+  console.log('prcess env', process.env.SERVICE_ID);
   const handleSendMessage = useCallback(
     async (event: React.FormEvent<HTMLFormElement>) => {
       event.preventDefault();
@@ -36,6 +47,25 @@ const ContactForm: FC = memo(() => {
        * This is a good starting point to wire up your form submission logic
        * */
       console.log('Data to send: ', data);
+      console.log('USER_ID', process.env.USER_ID);
+      emailjs
+        .sendForm(process.env.SERVICE_ID!, process.env.TEMPLATE_ID!, event.currentTarget, process.env.USER_ID)
+        .then(result => {
+          console.log('result', result);
+          Success();
+          window.location.reload();
+        })
+        .catch(err => {
+          console.log(err);
+          return (
+            <div role="alert">
+              <div className="rounded-t bg-red-500 px-4 py-2 font-bold text-white">Error</div>
+              <div className="rounded-b border border-t-0 border-red-400 bg-red-100 px-4 py-3 text-red-700">
+                <p>Unable to Sent Email</p>
+              </div>
+            </div>
+          );
+        });
     },
     [data],
   );
@@ -45,11 +75,11 @@ const ContactForm: FC = memo(() => {
 
   return (
     <form className="grid min-h-[320px] grid-cols-1 gap-y-4" method="POST" onSubmit={handleSendMessage}>
-      <input className={inputClasses} name="name" onChange={onChange} placeholder="Name" required type="text" />
+      <input className={inputClasses} name="from_name" onChange={onChange} placeholder="Name" required type="text" />
       <input
         autoComplete="email"
         className={inputClasses}
-        name="email"
+        name="from_email"
         onChange={onChange}
         placeholder="Email"
         required
